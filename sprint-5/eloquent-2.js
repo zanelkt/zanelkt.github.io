e08856faaa6155ad1697a7e808e9ea4ba46e7efb
zanelkt <zanelockett@gmail.com>
2018-07-10 12:24:54 +1200

    sprint 5.1

---
+++ sprint-5/eloquent-2.js
@@ -0,0 +1,32 @@
+LOOPING A TRIANGLE
+
+for (let line = "#"; line.length < 8; line += "#")
+  console.log(line);
+
+FIZZBUZZ
+
+for (let n = 1; n <= 100; n++) {
+    let output = "";
+    if (n % 3 == 0) output += "Fizz";
+    if (n % 5 == 0) output += "Buzz";
+    console.log(output || n);
+  }
+
+CHESSBOARD
+
+let size = 8;
+
+let board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board);
